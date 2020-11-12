2cf8ceb06618a9cb5333388a4e0b448f641c1d51
NatrezC <cnatrez@gmail.com>
2020-11-12 09:13:17 -0600

    can log in and log out fixed found.user error

--- config/ppConfig.js
+++ config/ppConfig.js
@@ -21,10 +21,8 @@ passport.deserializeUser((id, doneCallback)=>{
 /*
  This is Passport's strategy to provide local authentication. We provide the
  following information to the LocalStrategy:
-
  Configuration: An object of data to identify our authentication fields, the
  username and password
-
  Callback function: A function that's called to log the user in. We can pass
  the email and password to a database query, and return the appropriate
  information in the callback. Think of "doneCallback" as a function that'll later look
